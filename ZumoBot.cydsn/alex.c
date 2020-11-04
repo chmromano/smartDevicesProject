8a0380dd81acea3d8090f94468f1f7cc6ad9e1a4
Xiaoming.Ma <xiaoming.ma@metropolia.fi>
2020-11-04 13:21:12 +0200

    a series of tank motion function and zmain function of week 1 assigment 3 added.

--- ZumoBot.cydsn/alex.c
+++ ZumoBot.cydsn/alex.c
@@ -11,5 +11,80 @@
 */
 #include "alex.h"
 
+#if 0
+int zmain (void)
+{
+    motor_start();
+    Ultra_Start();
+    vTaskDelay(1000);
+    
+    
+    while(true) {
+        int d = Ultra_GetDistance();
+        if (d>10) {
+        motor_forward50();
+         
+    }
+    else {
+        motor_littleback();
+        tank_randomturn();
+        motor_forward50();
+    }
+        printf("distance = %d\r\n", d);
+        vTaskDelay(200);
+    }  
+}
+#endif
+
+
+
+
+
+
+void tank_turn(int8 speed, uint32 delay)
+{
+    SetMotors(0,1, speed, speed, delay);
+}
+void tank_leftturn90(void)
+{
+    SetMotors(0,1, 25, 25, 1050);
+}
+void tank_rightturn90(void)
+{
+    SetMotors(1,0, 25, 25, 1050);
+}
+void tank_leftturn270(void)
+{
+    SetMotors(0,1, 25, 25, 1050);
+}
+void tank_rightturn270(void)
+{
+    SetMotors(1,0, 25, 25, 1050);
+}
+void tank_turn180(void)
+{
+    SetMotors(0,1, 25, 25, 1050);
+}
+void motor_littleback(void)
+{
+    SetMotors(1,1, 15, 15, 1000);
+}
+
+void tank_randomturn(void)
+{
+    srand((unsigned)time(NULL));
+    int n = rand() % 2;
+    if (n==0) {
+        tank_leftturn90();
+    }
+    else {
+        tank_rightturn90();
+        }
+}
+void motor_forward50(void)
+{
+    SetMotors(0,0, 50, 50, 0);
+}
+
 /* [] END OF FILE */
 
