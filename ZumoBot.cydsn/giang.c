f0c14154209621527f73115bd38dc85473a5b9ca
giangv <giangv@metropolia.fi>
2020-11-26 01:01:31 +0200

    Line following Zumo project added to giang.c - will add more comments later

--- ZumoBot.cydsn/giang.c
+++ ZumoBot.cydsn/giang.c
@@ -13,6 +13,198 @@
 
 #if 0
     
+// Line follower
+// Apply codes from assignmnet 2 week 4 line following and adjust with more conditions
+// Bonus off track is made by adjust the reflectance threshold and motor turn variables
+
+#define READY "Zumo03/ready"
+#define START "Zumo03/start"
+#define STOP "Zumo03/stop"
+#define TIME "Zumo03/time"
+#define MISS "Zumo03/miss"
+#define LINE "Zumo03/line"
+
+#define STOPP 3
+
+void zmain(void)
+{
+    struct sensors_ dig;             //call function sensor
+    
+    reflectance_set_threshold(15000, 12000, 12000, 12000, 12000, 15000);       //set sensor threshold all to 10000
+    
+    reflectance_start();            //start reflectance sensor
+    IR_Start();                     //start IR receiver
+        
+    int slow_speed = 70;
+    int max_speed = 250;
+    int fast_turn_speed_1 = 120;
+    int fast_turn_speed_2 = 60;
+    int slow_turn_speed_1 = 50;
+    int slow_turn_speed_2 = 10;
+    int reverse_speed = 250;
+    bool reverse = false;
+    bool race = false;
+    bool finish = false;
+    
+    TickType_t start_time = xTaskGetTickCount();
+    TickType_t stop_time = xTaskGetTickCount();
+    int total_time = 0;
+    TickType_t miss_time = xTaskGetTickCount();
+    TickType_t back_to_line_time = xTaskGetTickCount();
+    
+    printf("Starting\n");
+
+    while (true)
+    {
+        int count = 0;
+        
+        motor_start();                  
+        motor_forward(0,0);             
+    
+        printf("Press to go to start line\n");
+        while(SW1_Read() == 1)                  //button not pressed
+        {
+            vTaskDelay(10);
+        }
+        BatteryLed_Write(1);            
+        BatteryLed_Write(0);            
+        
+        
+        reflectance_digital(&dig);      
+        motor_forward(slow_speed,0);            //Moving forward
+        
+        //loop until line counting = numbers of stop
+        //follow the line when 2 inside sensor on black area 
+        //stop when count equal stop means all sensors on black area
+        
+        //when R1, R2 is 1, go straight
+        //when R2 is 1, turn right until return to 0
+        //when L2 is 1, turn left until return to 0
+        
+        while(count < STOPP)             
+        {
+            reflectance_digital(&dig);
+            
+            //check when robot on black horizontal line
+            if (reverse == false && dig.R1 == 1 && dig.R2 == 1 && dig.R3 == 1 && dig.L1 == 1 && dig.L2 == 1 && dig.L3 == 1)
+            {
+                count++;
+                printf("count: %d\n", count);
+                
+                //for robot moves to first line and wait for button pressed
+                if (count == 1)
+                {
+                    motor_forward(0,0);
+                    if (finish == false)
+                    {
+                        print_mqtt(READY, "line");
+                    }
+                    while(SW1_Read() == 1)          
+                    {
+                        vTaskDelay(10);
+                    }
+                    race = true;
+                    motor_forward(max_speed,0);
+                    print_mqtt(START, "%d", start_time);
+                }
+                
+                //slow down when moving through the horizontal line so there is no error
+                while((dig.R1 == 1 && dig.R2 == 1 && dig.R3 == 1 && dig.L1 == 1 && dig.L2 == 1 && dig.L3 == 1) && count < STOPP)
+                {
+                    if (count > 1)
+                    {
+                        motor_forward(slow_speed, 0);
+                    }
+                    reflectance_digital(&dig);
+                }
+            }
+            
+            //start the loop from beginning when not in the race
+            //to avoid it turns before going to the start line
+            else if (race == false)
+            {
+                motor_forward(slow_speed,0);
+                continue;
+            }
+            
+            //when to go forward
+            else if (dig.R1 == 1 && dig.L1 == 1 && dig.R2 == 0 && dig.R3 == 0 && dig.L2 == 0 && dig.L3 == 0 )
+            {
+                if (reverse == true)
+                {
+                    reverse = false;
+                    back_to_line_time = xTaskGetTickCount();
+                    print_mqtt(LINE, "%d", back_to_line_time);
+                }
+                motor_forward(max_speed,0);
+            }
+            
+            //when to turn fast
+            else if ((dig.R2 == 1 || dig.R3 == 1) && dig.L1 == 1 && dig.L2 == 0 && dig.L3 == 0)
+            {
+                motor_turn(fast_turn_speed_1,fast_turn_speed_2,0);
+                //printf("turn right\n");
+            }
+            
+            //when to turn fast
+            else if ((dig.L2 == 1 || dig.L3 == 1) && dig.R1 == 1 && dig.R2 == 0 && dig.R3 == 0)
+            {
+                motor_turn(fast_turn_speed_2,fast_turn_speed_1,0);
+                //printf("turn left\n");
+            }
+            
+            //when to turn slow
+            else if ((dig.R2 == 1 || dig.R3 == 1) && dig.L2 == 0 && dig.L3 == 0)
+            {
+                motor_turn(slow_turn_speed_1,slow_turn_speed_2,0);
+                //printf("turn right\n");
+            }
+            
+            //when to turn slow
+            else if ((dig.L2 == 1 || dig.L3 == 1) && dig.R2 == 0 && dig.R3 == 0)
+            {
+                motor_turn(slow_turn_speed_2,slow_turn_speed_1,0);
+                //printf("turn left\n");
+            }
+            
+            //when off track
+            else if (dig.R1 == 0 && dig.L1 == 0)
+            {
+                if (race == false)
+                {
+                    motor_forward(slow_speed,0);
+                }
+                else if (reverse == false)
+                {
+                    miss_time = xTaskGetTickCount();
+                    print_mqtt(MISS, "%d", miss_time);
+                }
+                motor_backward(reverse_speed,0);
+                reverse = true;
+            }
+        }
+        
+        motor_forward(0,0);
+        stop_time = xTaskGetTickCount();
+        print_mqtt(STOP, "%d", stop_time);
+        
+        motor_stop();
+        total_time = stop_time - start_time;
+        print_mqtt(TIME, "%d", total_time);
+        printf("%d.%d\n", total_time/1000, total_time%1000);
+        finish = true;
+    }
+    
+     while(true)
+    {
+        vTaskDelay(100);
+    }
+}
+
+#endif
+
+#if 0
+    
 // Assignemnt 3 week 5 Robot sensor go straight line MQTT time
 
 // A loop of when button is pressed, robot moves forward and stops on the next black line
